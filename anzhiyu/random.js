40b2b60da3eb317b2985a70fbe1a53a32b708947
Fangqwe <1276243960@qq.com>
2026-07-05 16:33:53 +0800

    Site updated: YYYY-07-2026年7月5日 16:33:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/05/hello-world/"];function toRandomPost(){
+var posts=["2026/07/05/测试-1783240079771/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
